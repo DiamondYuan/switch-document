f5f8d3f7b99f4fb1d053499fe42264b4c05e5fc7
DiamondYuan <541832074@qq.com>
2018-03-10 12:24:31 +0800

    重构代码

--- background.js
+++ background.js
@@ -2,72 +2,74 @@
 
 var map;
 
-loadMap()
-chrome.browserAction.onClicked.addListener(iconClick);
+loadMap();
+
+function onClickedListener() {
+    handleCurrentTab(function (tab) {
+            var status = isInMap(tab.url);
+            if (status) {
+                chrome.tabs.update(tab.id, {url: status.url})
+            } else {
+                loadMap();
+            }
+        }
+    )
+}
+
+chrome.browserAction.onClicked.addListener(onClickedListener);
 chrome.tabs.onActivated.addListener(tabActivated);
 chrome.tabs.onUpdated.addListener(tabActivated);
 
-function iconClick() {
-  getCurrentTabUrl(goTo)
-};
 
 function tabActivated() {
-  getCurrentTabUrl(chaneIcon)
-};
+    handleCurrentTab(function (tab) {
+        var status = isInMap(tab.url);
+        if (status) {
+            chrome.browserAction.setIcon({path: status.icon});
+        } else {
+            chrome.browserAction.setIcon({path: 'default.png'});
+        }
 
-function goTo(url) {
-  for (var key in map) {
-    if (url.match(key)) {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
-        var currentUrl = tab[0].url;
-        chrome.tabs.update(tab.id, { url: currentUrl.replace(key, map[key]) });
-      });
-      return
-    }
-    if (url.match(map[key])) {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
-        var currentUrl = tab[0].url;
-        chrome.tabs.update(tab.id, { url: currentUrl.replace(map[key], key) });
-      });
-      return
-    }
-  }
-  loadMap()
+    })
 }
 
-function chaneIcon(url) {
-  console.log(url)
-  for (var key in map) {
-    if (url.match(key)) {
-      chrome.browserAction.setIcon({ path: "cn.png" });
-      return
-    }
-    if (url.match(map[key])) {
-      chrome.browserAction.setIcon({ path: 'en.png' });
-      return
-    }
-  }
-  chrome.browserAction.setIcon({ path: 'default.png' });
-}
-
-function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function (tabs) {
-    callback(tabs[0].url);
-  });
+function handleCurrentTab(callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+    chrome.tabs.query(queryInfo, function (tabs) {
+        callback(tabs[0]);
+    });
 }
 
 
-function loadMap() { 
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-      console.log("refresh data %s",xhr.responseText)
-      map = JSON.parse(xhr.responseText);
-  };
-  xhr.open('GET', 'https://raw.githubusercontent.com/DiamondYuan/switch-document/master/data.json');
-  xhr.send();
+function loadMap() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        map = JSON.parse(xhr.responseText);
+    };
+    xhr.open('GET', 'https://raw.githubusercontent.com/DiamondYuan/switch-document/master/data.json');
+    xhr.send();
 }
+
+function isInMap(url) {
+    for (var key in map) {
+        if (url.match(key)) {
+            return {
+                icon: "cn.png",
+                url: url.replace(key, map[key])
+            }
+        }
+        if (url.match(map[key])) {
+            return {
+                icon: "en.png",
+                url: url.replace(map[key], key)
+            }
+        }
+    }
+}
+
+
+
+
