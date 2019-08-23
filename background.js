f28372701ed47c2fc2e763122a4bb75786990773
DiamondYuan <admin@diamondyuan.com>
2019-08-23 11:23:55 +0800

    feat: rewrite

--- background.js
+++ background.js
@@ -1,75 +1,67 @@
-'use strict';
-
-var map;
-
-loadMap();
-
-function onClickedListener() {
-    handleCurrentTab(function (tab) {
-            var status = isInMap(tab.url);
-            if (status) {
-                chrome.tabs.update(tab.id, {url: status.url})
-            } else {
-                loadMap();
-            }
-        }
-    )
+class Loading {
+  start() {
+    chrome.browserAction.setIcon({ path: 'loading.gif' });
+  }
+  end() {
+    chrome.browserAction.setIcon({ path: 'default.png' });
+  }
 }
 
-chrome.browserAction.onClicked.addListener(onClickedListener);
-chrome.tabs.onActivated.addListener(tabActivated);
-chrome.tabs.onUpdated.addListener(tabActivated);
+const loading = new Loading();
+let map = {};
 
-
-function tabActivated() {
-    handleCurrentTab(function (tab) {
-        var status = isInMap(tab.url);
-        if (status) {
-            chrome.browserAction.setIcon({path: status.icon});
-        } else {
-            chrome.browserAction.setIcon({path: 'default.png'});
-        }
-
-    })
+function urlChangeHandler(url) {
+  var status = isInMap(url);
+  console.log('urlChangeHandler', status, url, map);
+  if (status) {
+    chrome.browserAction.setIcon({ path: status.icon });
+  } else {
+    chrome.browserAction.setIcon({ path: 'default.png' });
+  }
 }
 
-function handleCurrentTab(callback) {
-    var queryInfo = {
-        active: true,
-        currentWindow: true
-    };
-    chrome.tabs.query(queryInfo, function (tabs) {
-        callback(tabs[0]);
-    });
+async function loadData() {
+  loading.start();
+  const response = await fetch(
+    'https://raw.githubusercontent.com/DiamondYuan/switch-document/master/data.json',
+  );
+  loading.end();
+  map = await response.json();
 }
 
-
-function loadMap() {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        map = JSON.parse(xhr.responseText);
-    };
-    xhr.open('GET', 'https://raw.githubusercontent.com/DiamondYuan/switch-document/master/data.json');
-    xhr.send();
-}
+loadData();
 
 function isInMap(url) {
-    for (var key in map) {
-        if (url.match(key)) {
-            return {
-                icon: "cn.png",
-                url: url.replace(key, map[key])
-            }
-        }
-        if (url.match(map[key])) {
-            return {
-                icon: "en.png",
-                url: url.replace(map[key], key)
-            }
-        }
+  for (var key in map) {
+    if (url.match(key)) {
+      return {
+        icon: 'cn.png',
+        url: url.replace(key, map[key]),
+      };
     }
+    if (url.match(map[key])) {
+      return {
+        icon: 'en.png',
+        url: url.replace(map[key], key),
+      };
+    }
+  }
 }
 
+chrome.browserAction.onClicked.addListener(tab => {
+  var status = isInMap(tab.url);
+  if (status) {
+    chrome.tabs.update(tab.id, { url: status.url });
+  } else {
+    loadData();
+  }
+});
 
-
-
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  chrome.tabs.get(tabId, ({ url }) => {
+    urlChangeHandler(url);
+  });
+});
+chrome.tabs.onUpdated.addListener((_, { url }) => {
+  urlChangeHandler(url);
+});
